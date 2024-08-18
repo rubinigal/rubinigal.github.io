6cf76e8c79c4eaa169b4f774fcd8eb0ba8e4bd8e
Igal <rubin.igal.w@gmail.com>
2024-08-18 17:47:54 +0300

    added a temp home page + css
    footer css
    
    smaller file size
    
    added a temp home page + css + footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,18 @@
 import '../styles/Home.css'
+import videoFile from '../assets/EMVideo.mp4';
 
 function Home(): JSX.Element {
     return (
-    <div>
-        <h1>Home Page</h1>
-        <p>Welcome to the home page!</p>
-    </div>
+     <div className='video-container'>
+        <video autoPlay loop muted playsInline className='background-video'>
+            <source src={videoFile} type='video/mp4' />
+            Your browser does not support the video tag.
+        </video>
+        <div className='home-content'>
+            <h1>Home Page</h1>
+            <p>Welcome to the home page!</p>
+        </div>
+    </div> 
     )
 }
 
