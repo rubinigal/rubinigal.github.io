0461ad6a557160aac4ff8a1a058855a2c03d65ec
Igal <rubin.igal.w@gmail.com>
2024-08-18 17:14:48 +0300

    navbar css updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 //import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import './styles/App.css'
 
 function App(): JSX.Element {
   
@@ -11,7 +12,9 @@ function App(): JSX.Element {
     <Router>
       <Navbar/>
       <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/contact' element={<Contact />} />
       </Routes>
       <Footer/>
     </Router>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,16 @@
+import { Link } from 'react-router-dom';
+import '../styles/Navbar.css'
 
 function Navbar(): JSX.Element {
-    return (<></>)
+    return (
+    <nav className='nav-bar'>
+        <p><Link to='/'>RikmotAl Logo</Link></p> 
+        <ul>
+            <li><Link to='/about'>אודות</Link></li>
+            <li><Link to='/contact'>צרו קשר</Link></li>
+        </ul>
+    </nav>
+    )
 }
 
 export default Navbar
